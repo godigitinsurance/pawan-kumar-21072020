d56e37c9c2f151d7c2bdbd84ae3e61fcbf3c7c4a
godigitinsurance <59437973+godigitinsurance@users.noreply.github.com>
2020-07-20 20:55:28 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$("#customerName").text("Pawan Kumar");
+$("#policyNumber").text("EW21204783");
+$("#startDate").text("21-Jul-2020");
+$("#endDate").text("20-Jul-2021");
+
+
+$("#type").text("Two Wheeler");
+$("#registrationNumber").text("PB08EC0131");
+$("#make").text("BAJAJ");
+$("#model").text("CT 100");
+$("#vehicalSubType").text("Deluxe (99 CC)");
+$("#engineNo").text("DUYPJD35866");
+$("#chassis").text("MD2A18AY3JPD42391");
